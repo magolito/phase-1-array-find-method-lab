f7ce7bdeeae8254eb96c497343cd3160cc65a314
Manuel Avila <magolitonft2311@gmail.com>
2023-04-11 16:40:34 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    const winRecord = record.find(year => year.result === "W")
+    return winRecord ? winRecord.year : undefined 
+}
+
+
+
